1b5cd9992e7ae59f997e04735d43aae481931dfc
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2020-09-17 14:36:19 -0400

    Formatted Print Statement For Circle

--- point.py
+++ point.py
@@ -65,7 +65,7 @@ def main():
 
     circ = Circle(Point(-2,3), 2)
     p3 = Point(0, 1)
-    print("The circle with radius {} centered at the point ({}, {}) {} ({}, {}) and has an area of {} and a perimeter of {}.")
+    print("The circle with radius {} centered at the point ({}, {}) {} the point ({}, {}), and has an area of {} and a perimeter of {}.".format(circ.r, circ.p.x, circ.p.y, "contains" if circ.inside(p3) else "does not contain", p3.x, p3.y, circ.area(), circ.perimeter()))
 
 
 if __name__ == "__main__":
